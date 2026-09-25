dc8249f22c7daa485b15551c3a28e0eca9839107
dsazzz <tlqkf85914@gmail.com>
2026-09-25 19:12:14 +0900

    캐릭터 이미지 로드 및 중앙 출력 확인

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -8,7 +8,13 @@ def move_rectangle():
 def move_triangle():
     print('triangle')
 
-while True:
-    move_circle()
-    move_rectangle()
-    move_triangle()
+from pico2d import *
+
+open_canvas(800, 600)
+boy = load_image('character.png')
+
+clear_canvas()
+boy.draw(400, 300)
+update_canvas()
+delay(1)
+close_canvas()
